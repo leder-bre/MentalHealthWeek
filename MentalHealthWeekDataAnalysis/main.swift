0696fae3e99e4de580bb73837f78402b754e6410
rsgc-leder-b <bleder@rsgc.on.ca>
2017-01-12 14:01:57 -0500

    Initial Commit

---
+++ MentalHealthWeekDataAnalysis/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MentalHealthWeekDataAnalysis
+//
+//  Created by Brendan Leder on 2017-01-12.
+//  Copyright © 2017 Brendan Leder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
