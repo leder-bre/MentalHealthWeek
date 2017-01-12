ec474270093dfccc92ed85b1909a40d8fcc048a8
rsgc-leder-b <bleder@rsgc.on.ca>
2017-01-12 16:19:56 -0500

    Added code to remove whitespace from entries

--- MentalHealthWeekDataAnalysis/lineReader.swift
+++ MentalHealthWeekDataAnalysis/lineReader.swift
@@ -42,15 +42,3 @@ extension LineReader: Sequence {
         }
     }
 }
-
-// Read the text file (place in your home folder)
-// Path will probably be /Users/student/survey_response_sample.txt
-// Obtain the data file on Haiku, Day 37
-guard let reader = LineReader(path: "/Users/russellgordon/survey_response_sample.txt") else {
-    exit(0); // cannot open file
-}
-
-// Iterate over each line in the file and print to the terminal
-for line in reader {
-    print(">" + line.trimmingCharacters(in: .whitespacesAndNewlines))
-}
--- MentalHealthWeekDataAnalysis/main.swift
+++ MentalHealthWeekDataAnalysis/main.swift
@@ -9,24 +9,72 @@
 import Foundation
 
 struct Student {
-    var grade : Int
-    var advisor : String
-    var email : String
-    var monday : String
-    var tuesday : String
-    var wednesday : String
-    var thursday : String
-    var friday : String
-
-    init(grade : Int, advisor : String, email : String, monday : String, tuesday : String, wednesday : String, thursday : String, friday : String) {
-        self.grade = grade
-        self.advisor = advisor
-        self.email = email
-        self.monday = monday
-        self.tuesday = tuesday
-        self.wednesday = wednesday
-        self.thursday = thursday
-        self.friday = friday
-    }
+	var grade : Int
+	var advisor : String
+	var email : String
+	var monday : String
+	var tuesday : String
+	var wednesday : String
+	var thursday : String
+	var friday : String
+	
+	init(grade : Int, advisor : String, email : String, monday : String, tuesday : String, wednesday : String, thursday : String, friday : String) {
+		self.grade = grade
+		self.advisor = advisor
+		self.email = email
+		self.monday = monday
+		self.tuesday = tuesday
+		self.wednesday = wednesday
+		self.thursday = thursday
+		self.friday = friday
+	}
 }
 
+struct Activity {
+	var maxCount : Int
+	var assignedCount : Int
+	var name : String
+	
+	init(maxCount : Int, assignedCount : Int, name : String) {
+		self.maxCount = maxCount
+		self.assignedCount = assignedCount
+		self.name = name
+	}
+}
+
+var activities : [[Activity]]
+var students : [Student]
+
+
+// Read the text file
+guard let reader = LineReader(path: "/Users/student/survey_response_sample.txt") else {
+	print("File not found")
+	exit(0); // cannot open file
+}
+
+var firstIteration = true
+var secondIteration = true
+// Iterate over each line in the file and print to the terminal
+for line in reader {
+	var tempData : [String]
+	var tempGrade : Int
+	tempData = line.components(separatedBy: "\t")
+	if (tempData.count > 4) {
+		tempData.removeFirst(5)
+		if (firstIteration) {
+			firstIteration = false
+		} else if (secondIteration) {
+			secondIteration = false
+		} else {
+			tempGrade = Int(tempData[4])!
+			let upperBound = tempData.count-1
+			for i in 0...upperBound {
+				if (tempData[upperBound-i] == "") {
+					tempData.remove(at: upperBound-i)
+				}
+			}
+			print(tempData)
+			print("\n")
+		}
+	}
+}
