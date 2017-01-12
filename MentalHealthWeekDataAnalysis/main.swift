23759a6cf64f676c2ab7272186800167e229ef68
rsgc-leder-b <bleder@rsgc.on.ca>
2017-01-12 14:17:07 -0500

    created initial struct for student

--- MentalHealthWeekDataAnalysis/main.swift
+++ MentalHealthWeekDataAnalysis/main.swift
@@ -6,7 +6,25 @@
 //  Copyright © 2017 Brendan Leder. All rights reserved.
 //
 
-import Foundation
+struct Student {
+    var grade : Int
+    var advisor : String
+    var email : String
+    var monday : String
+    var tuesday : String
+    var wednesday : String
+    var thursday : String
+    var friday : String
 
-print("Hello, World!")
+    init(grade : Int, advisor : String, email : String, monday : String, tuesday : String, wednesday : String, thursday : String, friday : String) {
+        self.grade = grade
+        self.advisor = advisor
+        self.email = email
+        self.monday = monday
+        self.tuesday = tuesday
+        self.wednesday = wednesday
+        self.thursday = thursday
+        self.friday = friday
+    }
+}
 
